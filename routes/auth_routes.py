173a0bcaa7333067cce3411e1c90d6f97f1783d5
prabhhhuu <rsakthiprabhu2005@gmail.com>
2026-02-21 17:57:51 +0530

    Update auth_routes.py

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -4,21 +4,68 @@ from database import get_db
 
 auth_bp = Blueprint("auth", __name__)
 
-@auth_bp.route("/", methods=["GET", "POST"])
+
+# LOGIN PAGE
+@auth_bp.route("/")
+@auth_bp.route("/login", methods=["GET", "POST"])
 def login():
+
     if request.method == "POST":
+
         email = request.form["email"]
         password = request.form["password"]
 
         db = get_db()
-        user = db.execute("SELECT * FROM users WHERE email=?", (email,)).fetchone()
+
+        user = db.execute(
+            "SELECT * FROM users WHERE email=?",
+            (email,)
+        ).fetchone()
+
         db.close()
 
-        if user and check_password_hash(user[3], password):
-            session["user"] = user[1]
-            session["user_id"] = user[0]
+        if user and check_password_hash(user["password_hash"], password):
+
+            session["user"] = user["username"]
+
             return redirect("/editor")
 
         return "Invalid login"
 
     return render_template("login.html")
+
+
+# REGISTER PAGE
+@auth_bp.route("/register", methods=["GET","POST"])
+def register():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        hashed = generate_password_hash(password)
+
+        db = get_db()
+
+        db.execute(
+            "INSERT INTO users (username,email,password_hash) VALUES (?,?,?)",
+            (username,email,hashed)
+        )
+
+        db.commit()
+        db.close()
+
+        return redirect("/login")
+
+    return render_template("register.html")
+
+
+# LOGOUT
+@auth_bp.route("/logout")
+def logout():
+
+    session.clear()
+
+    return redirect("/login")
