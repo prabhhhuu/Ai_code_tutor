0d841097ce18c627ae45fcfeebe28193cf57fedc
prabhhhuu <rsakthiprabhu2005@gmail.com>
2026-02-21 17:50:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,12 @@ init_db()
 app.register_blueprint(auth_bp)
 app.register_blueprint(editor_bp)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
 
