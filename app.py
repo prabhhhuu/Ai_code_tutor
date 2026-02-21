2fb940ef2512f19485071eecf7742429cd1f791b
prabhhhuu <rsakthiprabhu2005@gmail.com>
2026-02-21 17:58:44 +0530

    Refactor app.run call for improved readability
    Refactor app.run to use multi-line format for clarity.

--- app.py
+++ app.py
@@ -14,8 +14,14 @@ app.register_blueprint(editor_bp)
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+
+    port = int(os.environ.get("PORT",5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
+
 
 
 
